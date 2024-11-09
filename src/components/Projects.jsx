6de4455fdd5446da6718aefaa43d6af219a9585d
Sadanand Miskin <miskinsadanand@gmail.com>
2024-11-09 17:38:11 +0530

    ""newproj

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,14 +5,16 @@ import hive from '../assets/Photos/hive.webp'
 import gitc from '../assets/Photos/gitc.webp'
 import xo from '../assets/Photos/XO.webp'
 import taskify from '../assets/Photos/taskify.webp'
+import notf from '../assets/Photos/notflix.webp'
+
 import { RiGithubFill } from "react-icons/ri";
 import { HiOutlineArrowTopRightOnSquare } from "react-icons/hi2";
 
 
 const Project = ({ title, description, imageUrl, technologies = [], index , githubLink, liveLink}) => {
     return (
-        <div 
-            className='project-card project-card-delay' 
+        <div
+            className='project-card project-card-delay'
             style={{ animationDelay: `${index * 0.2}s` }}
         >
             <div className='project-image'>
@@ -29,11 +31,11 @@ const Project = ({ title, description, imageUrl, technologies = [], index , gith
                 <div className='project-links'>
                     <a href={githubLink} target='_blank' className='project-link'><RiGithubFill size={40} />
                     </a>
-                    {liveLink!=null ? 
+                    {liveLink!=null ?
                     <a href={liveLink} target='_blank' className='project-link'><HiOutlineArrowTopRightOnSquare size={40}/>
-</a> 
+</a>
                     :
-                    <></>    
+                    <></>
                 }
                 </div>
             </div>
@@ -46,46 +48,56 @@ const Projects = () => {
 
     useEffect(() => {
         setProjects([
-            { 
-                title: "HiveDeal", 
-                description: "Collaborated on the development of an e-commerce website having Buyer, Admin privileges with Authencation. Adding functionalities like easy cart creation, adding, and removing items with a seamless checkout journey.", 
-                
-                githubLink: "https://github.com/SadanandMiskin/HiveDeal",
-                liveLink: "https://e-comm-fe.vercel.app/",
-                
-                imageUrl: hive, 
-                technologies: ["React", "Express", "MongoDB" , "Redux Toolkit"]
+            {
+                title: "NotFlix",
+                description: "Developed a Netflix clone with JWT authentication, advanced search, and YouTube trailer streaming using the TMDB API, with Redux for state management.",
+                githubLink: "https://github.com/SadanandMiskin/notflix",
+                liveLink: "https://notflixpage.vercel.app",
+                technologies: ["React", "Express", "MongoDB" , "Redux"],
+                imageUrl: notf
             },
-            { 
-                title: "XO Showdown", 
-                description: "Developed an Online Tic-Tac-Toe game using WebSockets, Node.js, and MongoDB for real-time gameplay.Enabled players to create a room with a unique ID and play with a friend or a random opponent online", 
+            {
+                title: "XO Showdown",
+                description: "Developed an Online Tic-Tac-Toe game using WebSockets, Node.js, and MongoDB for real-time gameplay.Enabled players to create a room with a unique ID and play with a friend or a random opponent online",
                 githubLink: "https://github.com/SadanandMiskin/XO-Showdown",
                 liveLink: "https://xoshowdown.vercel.app/",
-                imageUrl: xo, 
+                imageUrl: xo,
                 technologies: ["React", "Express", "WebSockets"]
             },
-            { 
-                title: "Taskify", 
-                description: "A full-stack Todo application that allows users to manage their tasks with features like authentication, Google sign-in, and CRUD operations.", 
+            {
+                title: "Taskify",
+                description: "A full-stack Todo application that allows users to manage their tasks with features like authentication, Google sign-in, and CRUD operations.",
                 githubLink: "https://github.com/SadanandMiskin/Taskify",
                 liveLink: "https://todo.sadanandmiskin.xyz/login",
-                imageUrl: taskify, 
+                imageUrl: taskify,
                 technologies: ["React", "Express", "MongoDB" , "JWT" , "Google Auth"]
             },
-            { 
-                title: "EmpVerify", 
-                description: "Web3 application for streamlined HR verification processes, ensuring transparency and tamper-proofing. Integrated IPFS for decentralized data storage, resulting in increase in data retrieval efficiency.", 
+            {
+                title: "HiveDeal",
+                description: "Collaborated on the development of an e-commerce website having Buyer, Admin privileges with Authencation. Adding functionalities like easy cart creation, adding, and removing items with a seamless checkout journey.",
+
+                githubLink: "https://github.com/SadanandMiskin/HiveDeal",
+                liveLink: "https://e-comm-fe.vercel.app/",
+
+                imageUrl: hive,
+                technologies: ["React", "Express", "MongoDB" , "Redux Toolkit"]
+            },
+
+
+            {
+                title: "EmpVerify",
+                description: "Web3 application for streamlined HR verification processes, ensuring transparency and tamper-proofing. Integrated IPFS for decentralized data storage, resulting in increase in data retrieval efficiency.",
                 imageUrl: empIm ,
                 githubLink: "https://github.com/sadanandmiskin/empverify",
                 liveLink: null,
                 technologies: ["Solidity", "Node.js", "MongoDB", "EJS", "Ganache"]
             },
-            
-            { 
-                title: "GitCrawler", 
-                description: "Watches new issues for any repository user provides and notifies via email", 
+
+            {
+                title: "GitCrawler",
+                description: "Watches new issues for any repository user provides and notifies via email",
                 githubLink: "https://github.com/SadanandMiskin/GitCrawler",
-                imageUrl: gitc, 
+                imageUrl: gitc,
                 technologies: ["Node", "Express", "NodeMailer", "EJS"]
             },
         ])
