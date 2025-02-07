28c78e0860116f02ee0b065a8e73286cd61195eb
Sadanand Miskin <miskinsadanand@gmail.com>
2025-02-07 19:08:57 +0530

    Update NameCard.tsx

--- src/components/homepage/NameCard.tsx
+++ src/components/homepage/NameCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CgArrowTopRight } from 'react-icons/cg';
 import { useLocation } from 'react-router-dom';
 
